5becd7f7c4eb1e16dc08e94ac13535748d8d5429
Bruno Viera <brunovierag@gmail.com>
2024-01-27 12:36:38 -0300

    improve button component

--- src/app/app/components/Buttons/Button.tsx
+++ src/app/app/components/Buttons/Button.tsx
@@ -1,12 +1,21 @@
 import classNames from "classnames";
 import { ReactNode } from "react";
 
+enum ButtonStyle {
+  primary = "bg-blue-500 text-blue-950 hover:bg-blue-400  focus-visible:ring-blue-500",
+  success = "bg-green-500 text-green-950 hover:bg-green-400  focus-visible:ring-green-500",
+  warning = "bg-yellow-500 text-yellow-950 hover:bg-yellow-400  focus-visible:ring-yellow-500",
+  danger = "bg-red-500 text-red-950 hover:bg-red-400  focus-visible:ring-red-500",
+}
+
 export default function Button({
   children,
   className = "",
   type = "button",
+  style = ButtonStyle.primary,
   onClick,
 }: {
+  style?: ButtonStyle;
   children: ReactNode;
   className?: string;
   type?: "button" | "submit";
@@ -17,7 +26,8 @@ export default function Button({
       type={type}
       onClick={onClick}
       className={classNames(
-        "font-bold py-2 px-4 rounded inline-flex items-center",
+        "inline-flex justify-center rounded-md border border-transparent  px-4 py-2 text-sm font-medium focus:outline-none focus-visible:ring-2  focus-visible:ring-offset-2",
+        style,
         className
       )}
     >
@@ -25,3 +35,8 @@ export default function Button({
     </button>
   );
 }
+
+Button.Primary = ButtonStyle.primary;
+Button.Success = ButtonStyle.success;
+Button.Warning = ButtonStyle.warning;
+Button.Danger = ButtonStyle.danger;
