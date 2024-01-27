a685076913dd6037ea8b752ece740390b9ed93a6
Bruno Viera <brunovierag@gmail.com>
2024-01-27 12:37:08 -0300

    create modal component

---
+++ src/app/app/components/Modal/index.tsx
@@ -0,0 +1,46 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { ReactNode } from "react";
+
+export default function Modal({
+  children,
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  children: ReactNode;
+  onClose: () => void;
+}) {
+  return (
+    <Transition appear show={isOpen} as="div">
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
+        <Transition.Child
+          as="div"
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as="div"
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              {children}
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
